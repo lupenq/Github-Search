aaa1b078f94c981710f45719ec8d34d1dfcc0d6a
lupenq <skinilya@gmail.com>
2020-04-02 01:54:57 +0300

    Updates

---
+++ precache-manifest.b6d95f1566fc2f5028f11e5e78b61d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b9f8f6eced2c667b4928ba65ce0e71",
+    "url": "/ghsearch/index.html"
+  },
+  {
+    "revision": "0b5e80440507e9688231",
+    "url": "/ghsearch/static/css/main.ff0d9e58.chunk.css"
+  },
+  {
+    "revision": "6411f03004d14e7aa9bd",
+    "url": "/ghsearch/static/js/2.9aa367eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ghsearch/static/js/2.9aa367eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5e80440507e9688231",
+    "url": "/ghsearch/static/js/main.6d97d11c.chunk.js"
+  },
+  {
+    "revision": "aee67eee0070d38ce464",
+    "url": "/ghsearch/static/js/runtime-main.f0a2282f.js"
+  }
+]);
