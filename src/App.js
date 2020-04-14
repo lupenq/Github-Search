bcbf94cdf29b03613070e14f8c66b37912d51f15
lupenq <skinilya@gmail.com>
2020-04-14 10:56:15 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {createBrowserHistory} from 'history'
+import {Navbar} from "./components/Navbar"
+import {Home} from "./pages/Home"
+import {About} from "./pages/About"
+import {Profile} from "./pages/Profile"
+import {Alert} from './components/Alert'
+import {AlertState} from './context/alert/AlertState'
+import {GithubState} from './context/github/GithubState'
 
 function App() {
+  // const history = createBrowserHistory()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert />
+              <Switch>
+                  <Route path="/" exact component={Home}/>
+                  <Route path="/about" component={About}/>
+                  <Route path="/profile/:name" component={Profile}/>
+              </Switch>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 }
 
